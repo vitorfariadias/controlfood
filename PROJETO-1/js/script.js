eff4224e2e65d7cddbe35368d1c65609d1bc53d3
KulluaXX <samuel.ngbr@gmail.com>
2021-10-05 16:58:09 -0300

    Adicao de novos arquivos

---
+++ PROJETO-1/js/script.js
@@ -0,0 +1,7 @@
+document.getElementById('add').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.getElementById('close_pop').addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+});
