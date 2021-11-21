717521fe7291d7c4a296fe56c7f8c981526c1c82
Vitor <vitorfariadias@gmail.com>
2021-11-21 17:50:38 -0300

    add: estrutura basica da pagina de produtos

--- src/Products/index.tsx
+++ src/Products/index.tsx
@@ -1,10 +1,49 @@
 import '../stylesheets/products.css';
+import { ReactComponent as Delete } from '../images/delete.svg';
+import { ReactComponent as Edit } from '../images/edit.svg';
+import { useEffect } from 'react';
 
 function Products() {
+
+  useEffect(() => {
+    console.log('useEffect funcionado!');
+  }, []);
+
+
   return (
-    <div>
-      <h1>produtos</h1>
-    </div>
+    <>
+      <section className="products-content">
+        <div className="products-card">
+          <h1>Produtos</h1>
+          <table className="products-table">
+            <thead>
+              <tr>
+                <th>nome</th>
+                <th>descricao</th>
+                <th>valorVenda</th>
+                <th>valorCusto</th>
+                <th>Materia-prima(lista)</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody className="products-table__row">
+              <tr>
+                <td>nome do produto</td>
+                <td>descricao do produto</td>
+                <td>R$ 12,00</td>
+                <td>R$ 2,00</td>
+                <td>lista de matéria-prima</td>
+                <td>
+                  <Delete />
+                  <Edit />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <button className="products-add">Adicionar</button>
+        </div>
+      </section>
+    </>
   );
 }
 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,22 +1,18 @@
-import {
-    BrowserRouter,
-    Routes,
-    Route
-  } from "react-router-dom";
-import Delivery from "./Delivery";
-  import Home from './Home';
-import Navbar from "./Navbar";
-import Products from "./Products";
-import User from "./User";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Delivery from './Delivery';
+import Home from './Home';
+import Navbar from './Navbar';
+import Products from './Products';
+import User from './User';
 
 function RoutesFunction() {
   return (
     <BrowserRouter>
-    <Navbar/>
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/delivery" element={<Delivery />}/>
+        <Route path="/delivery" element={<Delivery />} />
         <Route path="/user" element={<User />} />
       </Routes>
     </BrowserRouter>
