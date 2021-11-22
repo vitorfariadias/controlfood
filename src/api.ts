8e10024dd9a06ba454bc0c6d5d5ef1c45dd17c00
Vitor <vitorfariadias@gmail.com>
2021-11-22 08:14:12 -0300

    fix: altera url de comunicacao com api swagger

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://control-food-api.herokuapp.com';
 
 export function fetchProducts(){
     return axios(`${API_URL}/products`)
